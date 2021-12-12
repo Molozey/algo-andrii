ad0e4fb44cd7b33ae329b9dcaf61cf4d9efae381
Molozey <lipskii.as@phystech.edu>
2021-12-12 06:51:48 +0300

    Add: Similar companies

--- forta/FIFA_def.py
+++ forta/FIFA_def.py
@@ -1,6 +1,8 @@
 import pymysql as SQL
 import pandas as pd
 import numpy as np
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
 
 Host = "82.148.19.206"
 User = "molozey"
@@ -33,7 +35,6 @@ def FIFA(ticker_name:str, columns:list):
         else:
             NewValues = NewMin
         return NewValues
-
     ret_dict = dict()
     ret_dict.update({'ticker': ticker_name})
     for COL in columns:
@@ -64,5 +65,56 @@ def FIFA(ticker_name:str, columns:list):
 
     return ret_dict
 
-print(list(FIFA('AACG', columns=['market_cap', 'earnings', 'revenue_growth', 'price_to_earnings']).values())[1:])
-print(FIFA('AACG', columns=['market_cap', 'earnings', 'revenue_growth', 'price_to_earnings'])["market_cap"])
+# print(list(FIFA('AACG', columns=['market_cap', 'earnings', 'revenue_growth', 'price_to_earnings']).values())[1:])
+# print(FIFA('AACG', columns=['market_cap', 'earnings', 'revenue_growth', 'price_to_earnings'])["market_cap"])
+
+
+
+def get_similar(ticker, Host, User, Password, DataBaseName):
+    """
+
+    :param ticker: Name ticker to find similar tickers
+    :param Host: DataBase Host
+    :param User: DataBase User
+    :param Password: DataBase Password
+    :param DataBaseName: DataBase Name
+    :return: list of similar tickers
+    """
+    db = SQL.connect(host=Host, user=User, password=Password, database=DataBaseName)
+    cursor = db.cursor()
+
+    INFO = cursor.execute("""SELECT Indicators.*, Sector.Sector, Info.Country
+                                FROM Ticker_Information as Info
+                                JOIN Ticker_Sector as Sector USING (Ticker)
+                                JOIN Ticker_Indicators as Indicators USING (Ticker)""")
+    INFO = cursor.fetchall()
+    INFO_COLUMNS = [_[0] for _ in cursor.description]
+    db.close()
+
+    #   SIMILAR NEIGHBORS
+
+    DF_INFO = pd.DataFrame(INFO, columns=INFO_COLUMNS)
+    scaler = MinMaxScaler()
+    DF_INFO[['Last_Price', 'Market_Cap', 'Earnings', 'Revenue_Growth','Price_To_Earnings']] = scaler.fit_transform(DF_INFO[['Last_Price', 'Market_Cap', 'Earnings', 'Revenue_Growth','Price_To_Earnings']])
+    encode = LabelEncoder()
+    DF_INFO['Country'] = encode.fit_transform(DF_INFO['Country'])
+    DF_INFO['Sector'] = encode.fit_transform(DF_INFO['Sector'])
+
+    weights = [0.05, 0.05, 0.05, 0.1, 0.15, 0.3, 0.2]
+    #neighbors = NearestNeighbors(n_neighbors=5, radius=1, metric_params={})
+    neighbors = NearestNeighbors(algorithm='brute',
+                            metric='wminkowski',
+                            metric_params={'w': weights},
+                            p=1,
+                            n_jobs=1,
+                            n_neighbors=5)
+    neighbors.fit(DF_INFO.replace(np.NAN, -1)[['Last_Price', 'Market_Cap', 'Earnings', 'Revenue_Growth','Price_To_Earnings', 'Sector', 'Country']].values)
+    TICKERS_LABELS = DF_INFO.Ticker
+    obj = DF_INFO[DF_INFO.Ticker == ticker].replace(np.NAN, -1)[['Last_Price', 'Market_Cap', 'Earnings', 'Revenue_Growth','Price_To_Earnings', 'Sector', 'Country']].values[0]
+    dist, labels = neighbors.kneighbors([obj], n_neighbors=5)
+    DF_INFO = DF_INFO.iloc[labels[0]]
+    return DF_INFO.iloc[1:].Ticker.values
+
+
+# #[0.0015661645172121447 6.81310402006617e-06 0.04607668975686084 0.37574404761904767 -1 5 2]
+# get_similar('AACG', Host=Host, Password=Password, User=User, DataBaseName=DataBaseName)
--- forta/views.py
+++ forta/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from .models import *
 from .SQL_db_test import *
 from .forms import *
-from .FIFA_def import FIFA
+from .FIFA_def import FIFA, get_similar
+
+
 
 # Create your views here.
 def main(request):
@@ -17,6 +19,11 @@ def main(request):
     return render(request, 'base_main.html', context)
 
 def company_info(request, pk):
+     Host = "82.148.19.206"
+     User = "molozey"
+     Password = "Exeshnik08!"
+     DataBaseName = "FinTechData"
+
      companies_info = TickerInformation.objects.filter(ticker=pk)
      companies_sector = TickerSector.objects.filter(ticker=pk)
      fifa_data_tick = list(FIFA(ticker_name=pk, columns=['market_cap', 'earnings', 'revenue_growth', 'price_to_earnings']).values())
@@ -25,6 +32,16 @@ def company_info(request, pk):
          if company.logo_url == None:
              company.logo_url = 'https://www.dobro38.ru/upload/iblock/629/keg-20l.jpg'
 
-     context = {'companies_info': companies_info, 'companies_sector': companies_sector, 'fifa_data': fifa_data}
+     similar = list(get_similar(ticker=pk, Host=Host, Password=Password, User=User, DataBaseName=DataBaseName))
+     cmp_similar = list()
+     for _ in similar:
+         rows = TickerInformation.objects.filter(ticker=_)
+         for row in rows:
+             cmp_similar.append(row)
+
+
+     context = {'companies_info': companies_info, 'companies_sector': companies_sector,
+                'fifa_data': fifa_data,
+                'similar_companies': cmp_similar}
      return render(request, 'base_comp_info.html', context)
 
