3b441c081b74958fdf8f99c13359b70b531be9c3
J1zz <maltsev.oiu@phystech.edu>
2021-12-12 17:15:00 +0300

    ADD: some decors

--- wishlists/views.py
+++ wishlists/views.py
@@ -5,12 +5,31 @@ from forta.models import TickerInformation
 # Create your views here.
 
 def view(request):
-    wishlists = Wishlist.objects.all()
-    context = {"wishlists": wishlists}
+    try:
+        the_id = request.session['wishlist_id']
+    except:
+        the_id = None
+    if the_id:
+        wishlist = Wishlist.objects.get(id=the_id)
+        context = {"wishlist": wishlist}
+    else:
+        empty_message = "Looks so empty..."
+        context = {"empty": True, "empty_message": empty_message}
     return render(request, 'base_wishlist.html', context)
 
 def update_wishlist(request, pk):
-    wishlist = Wishlist.objects.all()[0]
+    ###################################### здесь автоматом удаляется сессия после 5 мин
+    request.session.set_expiry(300)
+    try:
+        the_id = request.session['wishlist_id']
+    except:
+        new_wishlist = Wishlist()
+        new_wishlist.save()
+        request.session['wishlist_id'] = new_wishlist.id
+        the_id = new_wishlist.id
+
+    wishlist = Wishlist.objects.get(id=the_id)
+
     try:
         company = TickerInformation.objects.get(ticker=pk)
     except TickerInformation.DoesNotExist:
