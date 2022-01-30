ee8e88675bbcf802a432810d8727b5569b7481c1
Molozey <lipskii.as@phystech.edu>
2022-01-30 09:26:49 +0300

    STARTED: New method to extract data

--- Andrii/Dir_Task_JAN_FEB/AvailableErrors.py
+++ Andrii/Dir_Task_JAN_FEB/AvailableErrors.py
@@ -12,13 +12,25 @@ class StrategyErrors:
         self.message = None
 
     def UnCompatibleFilter(self, Strategy, Filter):
-        self.message = f"Filter ({Filter.FilterIndex()}) not compatible with strategy ({Strategy.StrategyIndex()})"
+        self.message = f"Filter ({Filter.FilterIndex()}) not compatible with Extractor ({Strategy.ExtractorIndex()})"
         raise CompatibleException(self.message)
 
     def WrongIndicesType(self):
         self.message = f"Change DataFrame index to pandas datetime format"
         raise TypeError(self.message)
 
+    def UnCompatibleExtractor(self, Strategy, StrategyExtractor):
+        self.message = f"Extractor ({StrategyExtractor.ExtractorIndex()}) not compatible with Strategy ({Strategy.StrategyIndex()})"
+        raise CompatibleException(self.message)
+
+
+class SplitDataErrors:
+    def __init__(self):
+        self.message = None
+
+    def WrongCentralDotTypeError(self):
+        self.message = f"Central splitter dot have wrong type. Must be int"
+        raise TypeError(self.message)
 """--------------------------------------------------------------------------------------------------------------------------------------------------"""
 
 class FilterErrors:
