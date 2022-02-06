bd6f7d2557a26c61d6e70092b5d96e3d55728843
Molozey <lipskii.as@phystech.edu>
2022-02-06 19:59:13 +0300

    NodesConstructor: little pretify

--- Constructor/knotslinkers.py
+++ Constructor/knotslinkers.py
@@ -1,36 +1,6 @@
 from typing import List
 from abc import ABC, abstractmethod
-
-
-class BaseObjectType(ABC):
-
-    def __init__(self):
-        self._object = None
-
-    @property
-    @abstractmethod
-    def ObjectType(self):
-        pass
-
-    @abstractmethod
-    def transfer(self, obj):
-        pass
-
-    @abstractmethod
-    def receive(self):
-        pass
-
-
-class Value(BaseObjectType):
-
-    def ObjectType(self):
-        return self.__class__.__name__
-
-    def transfer(self, value):
-        self._object = value
-
-    def receive(self):
-        return self._object
+from Constructor.objectTypes import BaseObjectType
 
 
 class BaseOutputKnot(ABC):
