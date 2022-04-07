5de0c144fe756d7da0845617270a6332f9ed4e55
SteamedInvestors <93881755+SteamedInvestors@users.noreply.github.com>
2022-04-07 21:48:38 +0400

    ‘ get_fx_quote’ has been created

--- RealTimeTrade/connectorInterface.py
+++ RealTimeTrade/connectorInterface.py
@@ -13,37 +13,58 @@ import json
 class AbstractOrderInterface:
     def __init__(self):
         token = "eyJhbGciOiJFUzI1NiIsIng1dCI6IkRFNDc0QUQ1Q0NGRUFFRTlDRThCRDQ3ODlFRTZDOTEyRjVCM0UzOTQifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoiVG1XWGlqam1ZdFk0ZmF0MkIwZDdYdz09IiwiY2lkIjoiVG1XWGlqam1ZdFk0ZmF0MkIwZDdYdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiNmRkMjBkMGYwYWI3NDZjZWE1NThiMzc1NWM3MzI4ZDkiLCJkZ2kiOiI4NCIsImV4cCI6IjE2NDk0MjEzOTMiLCJvYWwiOiIxRiJ9.Q0Y6xfW_2Hetns3SsRhV3LrGyddDW2TLeM5NUiGdGHw-TeXPa0M4SYWVU8FcifzmYLVm8PNv07n6TDZ2_T6wCg"
-        
-#     @abstractmethod
-#     def instrument_to_uic(client, AccountKey, list_instruments):
-#         '''
-#         return: {'CHFJPY': 8, 'EURUSD': 21, ...} 
-#         '''
-        
-#         dict_uics = {}
-#         for instrument in list_instruments:
-#             spec = {'Instrument': [instrument]}
-#             try:
-#                 dict_uics[instrument] = list(InstrumentToUic(client, AccountKey, spec=spec).values())[0]
-#             except Exception as error:
-#                 print(error)
-#         return dict_uics
-    
+            
     @abstractmethod
-    def get_actual_data(self):
-        pass
+    def get_fx_quote(list_tickers):
+        '''
+        return a dict with elements like (where ticker_n is ticker and a key for the dictinary):
+            ticker_n : {'AssetType': 'FxSpot',
+                        'LastUpdated': '2022-04-07T17:25:09.946000Z',
+                        'PriceSource': 'SBFX',
+                        'Quote': {'Amount': 100000,
+                        'Ask': 132.797,
+                        'Bid': 132.757,
+                        'DelayedByMinutes': 0,
+                        'ErrorCode': 'None',
+                        'MarketState': 'Open',
+                        'Mid': 132.777,
+                        'PriceSource': 'SBFX',
+                        'PriceSourceType': 'Firm',
+                        'PriceTypeAsk': 'Tradable',
+                        'PriceTypeBid': 'Tradable'},
+                        'Uic': 8}
+        '''
+        # transfer tickers into Uids:
+        str_uics = ''
+        for ticker in list_tickers:
+            try:
+                uic = str(list(InstrumentToUic(client, AccountKey, spec={'Instrument': ticker}).values())[0]) + ','
+                str_uics += uic
+            except Exception as error:
+                print(f'{ticker}: {error}')
+        str_uics = str_uics[:-1]
+
+        params = {
+            "Uics": str_uics,
+            "AccountKey": AccountKey,
+            "AssetType": 'FxSpot'
+            }
+        # ask quotes:
+        r = tr.infoprices.InfoPrices(params)
+        # combine two lists in one dict:
+        return dict(zip(list_tickers, client.request(r)['Data']))
 
     @abstractmethod
     def place_open_order(client, AccountKey, dict_orders):
         '''
-        dict_orders = {fx_ticket: Amount}
-        fx_ticket: text
+        dict_orders = {fx_ticker: Amount}
+        fx_ticker: text
         Amount: -int, +int 
         '''
-        for ticket, amount in dict_orders.items():
+        for ticker, amount in dict_orders.items():
             try:
                 # find the Uic for Instrument
-                uic = list(InstrumentToUic(client, AccountKey, spec={'Instrument': ticket}).values())[0]
+                uic = list(InstrumentToUic(client, AccountKey, spec={'Instrument': ticker}).values())[0]
                 order = tie_account_to_order(AccountKey, MarketOrderFxSpot(Uic=uic, Amount=amount))
                 r = tr.orders.Order(data=order)
                 rv = client.request(r)
